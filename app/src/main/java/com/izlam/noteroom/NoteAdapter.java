343cac1e998dd9a79f0012f7df6020c87e7273f6
Islam <isalm9090@gmail.com>
2020-08-20 02:28:19 +0200

    Note App in RoomDB

---
+++ app/src/main/java/com/izlam/noteroom/NoteAdapter.java
@@ -0,0 +1,92 @@
+package com.izlam.noteroom;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+    private List<Note> notes = new ArrayList<>();
+    private OnItemClickListener listener;
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note note = notes.get(position);
+
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewPriority.setText(String.valueOf(note.getPriority()));
+
+
+    }
+
+    public Note getNoteAt(int position) {
+        return notes.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle;
+        private TextView textViewPriority;
+        private TextView textViewDescription;
+
+        public NoteHolder(@NonNull final View itemView) {
+
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+            itemView.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null&& position!= RecyclerView.NO_POSITION){
+                        listener.onItemClick(notes.get(position));
+                    }
+                }
+            });
+//            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//                @Override
+//                public boolean onLongClick(View v) {
+//                    listener.onItemLongClik(notes.get(getAdapterPosition()));
+//                    return true;
+//                }
+//            });
+        }
+    }
+
+    interface OnItemClickListener {
+        void onItemClick(Note note);
+//        void onItemLongClik(Note note);
+    }
+
+    public void setOnClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+}
